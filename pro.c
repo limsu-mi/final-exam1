bac10579c20497865d090af550134f4782ca7eea
limsu-mi <limsu-mi>
2017-12-19 10:50:37 +0900

    pro.c

---
+++ pro.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+struct student
+{
+    num;
+    printf("name : %s \n" , name);
+    printf("kor : %d \n", kor);
+    printf("mat : %d \n", mat);
+    printf("eng : %d \n", eng);
+};
+
+int main()
+{
+    int num;
+    char name[15];
+    char *nugu;
+    int kor;
+    int mat;
+    int eng;
+
+/*    scanf("name : %d \n", &num);
+    //getchar();
+    scanf("%s", name);
+    //getchar();
+    scanf("%d %d %d \n", &kor, &mat, &eng);
+    //getchar();
+*/
+    student:
+    {
+    scanf("%d", &num);
+    //getchar();
+    scanf("%s", name);
+    //getchar();
+    scanf("%d %d %d", &kor, &mat, &eng);
+    //getchar();
+    };
+    return 0;
+}
+
+
